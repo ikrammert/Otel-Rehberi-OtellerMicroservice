41855b81969edfcdc4711cd47755f939735a71aa
İkram MERT <129231962+ikrammert@users.noreply.github.com>
2024-03-03 21:41:01 +0300

    migrations

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -5,33 +5,67 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
+	_ "github.com/golang-migrate/migrate/v4/source/file" // Bu satırı ekleyin
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func DBinstance() *mongo.Client {
-	MongoDB := "mongodb://localhost:27017" //burayı daha sonra ayarlayacağız
-	fmt.Print(MongoDB)
+var Client *mongo.Client = DBinstance()
 
-	//mongo.NewClient(options.Client().ApplyURI(MongoDB))
+func migrateDatabase() error {
+	// Migrate işlemleri için veritabanı URL'sini belirtin
+	// Migrate işlemleri için source dizinini belirtin
+	migrationURL := "mongodb://localhost:27017/oteller"
+	sourceURL := "file://db"
+
+	// Migrate instance'ını oluşturun
+	// p := &mongodb.Mongo{}
+	// d, err := p.Open(migrationURL)
+	// if err != nil {
+	// 	return err
+	// }
+	log.Print("1")
+	m, err := migrate.New(sourceURL, migrationURL)
+	// m, err := migrate.NewWithDatabaseInstance(sourceURL, "oteller", d)
+	if err != nil {
+		log.Print("2")
+		return err
+	}
+	log.Print("burası")
+	// Migrate işlemlerini gerçekleştirin
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		log.Print("3")
+		return err
+	}
+
+	fmt.Println("Migration işlemi başarıyla tamamlandı")
+	return nil
+}
+
+func DBinstance() *mongo.Client {
+	MongoDB := "mongodb://localhost:27017" // MongoDB bağlantı URL'si burada
+	fmt.Println("Connecting to MongoDB:", MongoDB)
+
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(MongoDB))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	// defer cancel()
-
-	// //err = client.Connect(ctx)
 	fmt.Println("Connected to MongoDB")
 	return client
-
 }
 
-var Client *mongo.Client = DBinstance()
-
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	var Collection *mongo.Collection = client.Database("oteller").Collection(collectionName)
+	// Migration işlemlerini gerçekleştirin
+	err := migrateDatabase()
+	if err != nil {
+		log.Fatal("Migration işlemi sırasında hata oluştu:", err)
+	}
 
-	return Collection
+	// Belirtilen koleksiyonu açın
+	collection := client.Database("oteller").Collection(collectionName)
+	return collection
 }
